96e6073006374c00bcc8953b2ec585771d9f8367
NikVolf <nikvolf@gmail.com>
2017-04-12 18:06:03 +0300

    gas utility in builder

--- runner/build.sh
+++ runner/build.sh
@@ -13,3 +13,10 @@ else
     # c/c++ can be compiled directly by emcc
     emcc $file -Os -s WASM=1 -s SIDE_MODULE=1 -o out/contract.wasm
 fi
+
+if [ ! -f ./../gas/target/release/gas ] && [ ! -f ./../gas/target/release/gas.exe ] then
+    echo "No gas utility, compile it in /gas folder with"
+    echo "cargo build --release"    
+fi
+
+./../gas/target/release/gas ./out/contract.wasm ./out/contract.wasm
