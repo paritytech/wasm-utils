43130dd5c2bad2e82119465aa657d23b5e701fbb
NikVolf <nikvolf@gmail.com>
2017-04-13 01:03:41 +0300

    rust contract sample

---
+++ samples/contract2.rs
@@ -0,0 +1,19 @@
+#![feature(link_args)]
+#![feature(lang_items)]
+#![feature(drop_types_in_const)]
+#![no_main]
+
+#[link_args = "-s WASM=1 -s NO_EXIT_RUNTIME=1 -s NO_FILESYSTEM=1"]
+extern {}
+
+static mut DATA: Option<Vec<u8>> = None;
+
+#[no_mangle]
+pub fn call() {
+    let mut vec = Vec::new();
+    unsafe { if let Some(ref v) = DATA { vec.extend(v); }; }
+    vec.push(1u8);
+    unsafe {
+        DATA = Some(vec);
+    }
+}
