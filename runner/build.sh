638019dc943f8e6bd25bf81c5bcd990660f41547
NikVolf <nikvolf@gmail.com>
2017-04-27 23:40:51 +0300

    updated build script

--- runner/build.sh
+++ runner/build.sh
@@ -10,15 +10,15 @@ then
     # Rust is compiled with rustc
     rustc $file -o out/contract.js -O --target wasm32-unknown-emscripten
 
+    # Gas injector
+    cargo run --manifest-path=./../gas/Cargo.toml --release -- ./out/contract.wasm ./out/contract.wasm
+
     # Allocator replacer
     cargo run --manifest-path=./../ext/Cargo.toml --release -- ./out/contract.wasm ./out/contract.wasm
 
     # Symbols optimizer
     cargo run --manifest-path=./../opt/Cargo.toml --release -- ./out/contract.wasm ./out/contract.wasm
 
-    # Gas injector
-    cargo run --manifest-path=./../gas/Cargo.toml --release -- ./out/contract.wasm ./out/contract.wasm
-    
 else
     # c/c++ can be compiled directly by emcc
     emcc $file -Os -s WASM=1 -s SIDE_MODULE=1 -o out/contract.wasm
