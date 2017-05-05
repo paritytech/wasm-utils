688d652fcb3757c27196144e6c5dc237635ea187
NikVolf <nikvolf@gmail.com>
2017-05-05 16:06:16 +0300

    init log in executables

--- opt/src/main.rs
+++ opt/src/main.rs
@@ -5,6 +5,8 @@ use std::env;
 
 fn main() {
 
+    wasm_utils::init_log();
+
     let args = env::args().collect::<Vec<_>>();
     if args.len() < 3 {
         println!("Usage: {} input_file.wasm output_file.wasm", args[0]);
