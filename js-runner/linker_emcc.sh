050c0b76a835d4eb3f7c3ac0b0d42a4752c92ce7
NikVolf <nikvolf@gmail.com>
2017-05-30 13:53:18 +0300

    remove demos (moved to https://github.com/nikvolf/wasm-tools)

--- js-runner/linker_emcc.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-args="$*"
-filtered_args=${args/ERROR_ON_UNDEFINED_SYMBOLS\=1/ERROR_ON_UNDEFINED_SYMBOLS\=0}
-emcc $filtered_args 
--- samples/static_contract.rs
+++
@@ -1,8 +0,0 @@
-extern {
-  #[link(name="env")]
-  fn log_event(id: *const u8);
-}
-
-fn main() {
-    unsafe { log_event(::std::ptr::null()); }
-}
