73f8097117b139769bf0ef97e1732d533217fd98
Sergey Pepyakin <s.pepyakin@gmail.com>
2018-02-23 22:43:16 +0300

    Rename Context → Stack in stach_height.

--- src/stack_height/max_height.rs
+++ src/stack_height/max_height.rs
@@ -22,14 +22,16 @@ struct Frame {
 	start_height: u32,
 }
 
-struct Context {
+/// This is a compound stack that abstracts tracking height of the value stack
+/// and manipulation of the control stack.
+struct Stack {
 	height: u32,
 	control_stack: Vec<Frame>,
 }
 
-impl Context {
-	fn new() -> Context {
-		Context {
+impl Stack {
+	fn new() -> Stack {
+		Stack {
 			height: 0,
 			control_stack: Vec::new(),
 		}
@@ -145,7 +147,7 @@ pub fn max_stack_height(func_idx: u32, module: &elements::Module) -> u32 {
 	let body = &code_section.bodies()[func_idx as usize];
 	let opcodes = body.code();
 
-	let mut ctx = Context::new();
+	let mut stack = Stack::new();
 	let mut max_height: u32 = 0;
 	let mut pc = 0;
 
@@ -156,7 +158,7 @@ pub fn max_stack_height(func_idx: u32, module: &elements::Module) -> u32 {
 	} else {
 		0
 	};
-	ctx.push_frame(Frame {
+	stack.push_frame(Frame {
 		is_polymorphic: false,
 		end_arity: func_arity,
 		branch_arity: func_arity,
@@ -171,8 +173,8 @@ pub fn max_stack_height(func_idx: u32, module: &elements::Module) -> u32 {
 		// If current value stack is higher than maximal height observed so far,
 		// save the new height.
 		// However, we don't increase maximal value in unreachable code.
-		if ctx.height() > max_height && !ctx.frame(0).is_polymorphic {
-			max_height = ctx.height();
+		if stack.height() > max_height && !stack.frame(0).is_polymorphic {
+			max_height = stack.height();
 		}
 
 		let opcode = &opcodes.elements()[pc];
@@ -183,8 +185,8 @@ pub fn max_stack_height(func_idx: u32, module: &elements::Module) -> u32 {
 			Block(ty) | Loop(ty) | If(ty) => {
 				let end_arity = if ty == BlockType::NoResult { 0 } else { 1 };
 				let branch_arity = if let Loop(_) = *opcode { 0 } else { end_arity };
-				let height = ctx.height();
-				ctx.push_frame(Frame {
+				let height = stack.height();
+				stack.push_frame(Frame {
 					is_polymorphic: false,
 					end_arity,
 					branch_arity,
@@ -196,106 +198,106 @@ pub fn max_stack_height(func_idx: u32, module: &elements::Module) -> u32 {
 				// it as is.
 			}
 			End => {
-				let frame = ctx.pop_frame();
-				ctx.trunc(frame.start_height);
-				ctx.push_values(frame.end_arity);
+				let frame = stack.pop_frame();
+				stack.trunc(frame.start_height);
+				stack.push_values(frame.end_arity);
 			}
 			Unreachable => {
-				ctx.mark_unreachable();
+				stack.mark_unreachable();
 			}
 			Br(target) => {
 				// Pop values for the destination block result.
-				let target_arity = ctx.frame(target).branch_arity;
-				ctx.pop_values(target_arity);
+				let target_arity = stack.frame(target).branch_arity;
+				stack.pop_values(target_arity);
 
 				// This instruction unconditionally transfers control to the specified block,
 				// thus all instruction until the end of the current block is deemed unreachable
-				ctx.mark_unreachable();
+				stack.mark_unreachable();
 			}
 			BrIf(target) => {
 				// Pop values for the destination block result.
-				let target_arity = ctx.frame(target).branch_arity;
-				ctx.pop_values(target_arity);
+				let target_arity = stack.frame(target).branch_arity;
+				stack.pop_values(target_arity);
 
 				// Pop condition value.
-				ctx.pop_values(1);
+				stack.pop_values(1);
 
 				// Push values back.
-				ctx.push_values(target_arity);
+				stack.push_values(target_arity);
 			}
 			BrTable(ref targets, default_target) => {
-				let arity_of_default = ctx.frame(default_target).branch_arity;
+				let arity_of_default = stack.frame(default_target).branch_arity;
 
 				// Check that all jump targets have an equal arities.
 				debug_assert!({
 					targets
 						.iter()
-						.map(|target_rel_depth| ctx.frame(*target_rel_depth).branch_arity)
+						.map(|target_rel_depth| stack.frame(*target_rel_depth).branch_arity)
 						.all(|arity| arity == arity_of_default)
 				});
 
 				// Because all jump targets have an equal arities, we can just take arity of
 				// the default branch.
-				ctx.pop_values(arity_of_default);
+				stack.pop_values(arity_of_default);
 
 				// This instruction doesn't let control flow to go further, since the control flow
 				// should take either one of branches depending on the value or the default branch.
-				ctx.mark_unreachable();
+				stack.mark_unreachable();
 			}
 			Return => {
 				// Pop return values of the function. Mark successive instructions as unreachable
 				// since this instruction doesn't let control flow to go further.
-				ctx.pop_values(func_arity);
-				ctx.mark_unreachable();
+				stack.pop_values(func_arity);
+				stack.mark_unreachable();
 			}
 			Call(idx) => {
 				let ty = resolve_func_type(idx, module);
 
 				// Pop values for arguments of the function.
-				ctx.pop_values(ty.params().len() as u32);
+				stack.pop_values(ty.params().len() as u32);
 
 				// Push result of the function execution to the stack.
 				let callee_arity = if ty.return_type().is_some() { 1 } else { 0 };
-				ctx.push_values(callee_arity);
+				stack.push_values(callee_arity);
 			}
 			CallIndirect(x, _) => {
 				let Type::Function(ref ty) = type_section.types()[x as usize];
 
 				// Pop values for arguments of the function.
-				ctx.pop_values(ty.params().len() as u32);
+				stack.pop_values(ty.params().len() as u32);
 
 				// Push result of the function execution to the stack.
 				let callee_arity = if ty.return_type().is_some() { 1 } else { 0 };
-				ctx.push_values(callee_arity);
+				stack.push_values(callee_arity);
 			}
 			Drop => {
-				ctx.pop_values(1);
+				stack.pop_values(1);
 			}
 			Select => {
 				// Pop two values and one condition.
-				ctx.pop_values(2);
-				ctx.pop_values(1);
+				stack.pop_values(2);
+				stack.pop_values(1);
 
 				// Push the selected value.
-				ctx.push_values(1);
+				stack.push_values(1);
 			}
 			GetLocal(_) => {
-				ctx.push_values(1);
+				stack.push_values(1);
 			}
 			SetLocal(_) => {
-				ctx.pop_values(1);
+				stack.pop_values(1);
 			}
 			TeeLocal(_) => {
 				// This instruction pops and pushes the value, so
 				// effectively it doesn't modify the stack height.
-				ctx.pop_values(1);
-				ctx.push_values(1);
+				stack.pop_values(1);
+				stack.push_values(1);
 			}
 			GetGlobal(_) => {
-				ctx.push_values(1);
+				stack.push_values(1);
 			}
 			SetGlobal(_) => {
-				ctx.pop_values(1);
+				stack.pop_values(1);
 			}
 			I32Load(_, _)
 			| I64Load(_, _)
@@ -313,8 +315,8 @@ pub fn max_stack_height(func_idx: u32, module: &elements::Module) -> u32 {
 			| I64Load32U(_, _) => {
 				// These instructions pop the address and pushes the result,
 				// which effictively don't modify the stack height.
-				ctx.pop_values(1);
-				ctx.push_values(1);
+				stack.pop_values(1);
+				stack.push_values(1);
 			}
 
 			I32Store(_, _)
@@ -327,29 +329,29 @@ pub fn max_stack_height(func_idx: u32, module: &elements::Module) -> u32 {
 			| I64Store16(_, _)
 			| I64Store32(_, _) => {
 				// These instructions pop the address and the value.
-				ctx.pop_values(2);
+				stack.pop_values(2);
 			}
 
 			CurrentMemory(_) => {
 				// Pushes current memory size
-				ctx.push_values(1);
+				stack.push_values(1);
 			}
 			GrowMemory(_) => {
 				// Grow memory takes the value of pages to grow and pushes
-				ctx.pop_values(1);
-				ctx.push_values(1);
+				stack.pop_values(1);
+				stack.push_values(1);
 			}
 
 			I32Const(_) | I64Const(_) | F32Const(_) | F64Const(_) => {
 				// These instructions just push the single literal value onto the stack.
-				ctx.push_values(1);
+				stack.push_values(1);
 			}
 
 			I32Eqz | I64Eqz => {
 				// These instructions pop the value and compare it against zero, and pushes
 				// the result of the comparison.
-				ctx.pop_values(1);
-				ctx.push_values(1);
+				stack.pop_values(1);
+				stack.push_values(1);
 			}
 
 			I32Eq | I32Ne | I32LtS | I32LtU | I32GtS | I32GtU | I32LeS | I32LeU | I32GeS
@@ -357,16 +359,16 @@ pub fn max_stack_height(func_idx: u32, module: &elements::Module) -> u32 {
 			| I64GeS | I64GeU | F32Eq | F32Ne | F32Lt | F32Gt | F32Le | F32Ge | F64Eq | F64Ne
 			| F64Lt | F64Gt | F64Le | F64Ge => {
 				// Comparison operations take two operands and produce one result.
-				ctx.pop_values(2);
-				ctx.push_values(1);
+				stack.pop_values(2);
+				stack.push_values(1);
 			}
 
 			I32Clz | I32Ctz | I32Popcnt | I64Clz | I64Ctz | I64Popcnt | F32Abs | F32Neg
 			| F32Ceil | F32Floor | F32Trunc | F32Nearest | F32Sqrt | F64Abs | F64Neg | F64Ceil
 			| F64Floor | F64Trunc | F64Nearest | F64Sqrt => {
 				// Unary operators take one operand and produce one result.
-				ctx.pop_values(1);
-				ctx.push_values(1);
+				stack.pop_values(1);
+				stack.push_values(1);
 			}
 
 			I32Add | I32Sub | I32Mul | I32DivS | I32DivU | I32RemS | I32RemU | I32And | I32Or
@@ -376,8 +378,8 @@ pub fn max_stack_height(func_idx: u32, module: &elements::Module) -> u32 {
 			| F32Min | F32Max | F32Copysign | F64Add | F64Sub | F64Mul | F64Div | F64Min
 			| F64Max | F64Copysign => {
 				// Binary operators take two operands and produce one result.
-				ctx.pop_values(2);
-				ctx.push_values(1);
+				stack.pop_values(2);
+				stack.push_values(1);
 			}
 
 			I32WrapI64 | I32TruncSF32 | I32TruncUF32 | I32TruncSF64 | I32TruncUF64
@@ -387,8 +389,8 @@ pub fn max_stack_height(func_idx: u32, module: &elements::Module) -> u32 {
 			| F64PromoteF32 | I32ReinterpretF32 | I64ReinterpretF64 | F32ReinterpretI32
 			| F64ReinterpretI64 => {
 				// Conversion operators take one value and produce one result.
-				ctx.pop_values(1);
-				ctx.push_values(1);
+				stack.pop_values(1);
+				stack.push_values(1);
 			}
 		}
 		pc += 1;
