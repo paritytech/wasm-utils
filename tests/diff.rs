7fece0c580f3866d6c1624611236cc1f67a19cd7
Sergey Pepyakin <s.pepyakin@gmail.com>
2018-02-23 18:55:28 +0300

    Diff tests.

---
+++ tests/diff.rs
@@ -0,0 +1,84 @@
+extern crate diff;
+extern crate wasm_utils;
+extern crate wabt;
+extern crate parity_wasm;
+
+use std::fs;
+use std::io::{self, Read, Write};
+use std::path::{Path, PathBuf};
+use parity_wasm::elements;
+
+fn slurp<P: AsRef<Path>>(path: P) -> io::Result<Vec<u8>> {
+    let mut f = fs::File::open(path)?;
+    let mut buf = vec![];
+    f.read_to_end(&mut buf)?;
+    Ok(buf)
+}
+
+fn dump<P: AsRef<Path>>(path: P, buf: &[u8]) -> io::Result<()> {
+	let mut f = fs::File::create(path)?;
+	f.write_all(buf)?;
+	Ok(())
+}
+
+fn validate_wasm(binary: &[u8]) -> Result<(), wabt::Error> {
+	wabt::Module::read_binary(
+		&binary,
+		&Default::default()
+	)?.validate()?;
+	Ok(())
+}
+
+fn run_diff_test<F: FnOnce(&[u8]) -> Vec<u8>>(test_dir: &str, name: &str, test: F) {
+	let mut fixture_path = PathBuf::from(concat!(
+		env!("CARGO_MANIFEST_DIR"),
+		"/tests/fixtures/",
+	));
+	fixture_path.push(test_dir);
+	fixture_path.push(name);
+
+	let mut expected_path = PathBuf::from(concat!(
+		env!("CARGO_MANIFEST_DIR"),
+		"/tests/expectations/"
+	));
+	expected_path.push(test_dir);
+	expected_path.push(name);
+
+	let fixture_wat = slurp(&fixture_path).expect("Failed to read fixture");
+	let fixture_wasm = wabt::wat2wasm(fixture_wat).expect("Failed to read fixture");
+	validate_wasm(&fixture_wasm).expect("Fixture is invalid");
+
+	let expected_wat = slurp(&expected_path).unwrap_or(vec![]);
+	let expected_wat = String::from_utf8_lossy(&expected_wat);
+
+	let actual_wasm = test(fixture_wasm.as_ref());
+	validate_wasm(&actual_wasm).expect("Result module is invalid");
+
+	let actual_wat = wabt::wasm2wat(&actual_wasm).expect("Failed to convert result wasm to wat");
+	dump(&expected_path, actual_wat.as_bytes()).expect("Failed to write to expected");
+
+	if actual_wat != expected_wat {
+		println!("--- {}", expected_path.display());
+		println!("+++");
+		for diff in diff::lines(&expected_wat, &actual_wat) {
+			match diff {
+				diff::Result::Left(l) => println!("-{}", l),
+				diff::Result::Both(l, _) => println!(" {}", l),
+				diff::Result::Right(r) => println!("+{}", r),
+			}
+		}
+		panic!();
+	}
+}
+
+#[test]
+fn simple_wat() {
+	run_diff_test("stack-height", "simple.wat", |input| {
+		let rules = Default::default();
+		let module = elements::deserialize_buffer(input).expect("Failed to deserialize");
+		let instrumented = wasm_utils::stack_height::inject_stack_counter(module, &rules).expect("Failed to instrument with stack counter");
+		elements::serialize(instrumented).expect("Failed to serialize")
+	});
+}
+
+
