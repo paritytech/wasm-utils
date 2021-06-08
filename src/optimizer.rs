9f41646c6b90f7f1955b7699f22ebafeb3efd972
NikVolf <nikvolf@gmail.com>
2021-06-08 17:09:21 +0300

    strip custom sections

--- src/optimizer.rs
+++ src/optimizer.rs
@@ -330,6 +330,9 @@ pub fn optimize(
 		}
 	}
 
+	// Also drop all custom sections
+	module.sections_mut().retain(|section| if let elements::Section::Custom(_) = section { false } else { true });
+
 	Ok(())
 }
 
